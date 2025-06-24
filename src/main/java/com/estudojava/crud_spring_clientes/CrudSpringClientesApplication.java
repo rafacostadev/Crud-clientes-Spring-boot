e7a2049ba850987ed0a2d81ea15a91f726f48d23
rafacostadev <rafaelcostamantis@gmail.com>
2025-06-23 23:23:45 -0300

    inicial

---
+++ src/main/java/com/estudojava/crud_spring_clientes/CrudSpringClientesApplication.java
@@ -0,0 +1,13 @@
+package com.estudojava.crud_spring_clientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSpringClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringClientesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estudojava/crud_spring_clientes/CrudSpringClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudojava.crud_spring_clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSpringClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
